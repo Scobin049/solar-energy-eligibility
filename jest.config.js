62009ac5f435637a79658496ccccf7bfea7da805
Vitor Scobin <vhscobin@gmail.com>
2023-12-22 00:45:19 -0300

    chore: update jest configs

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.spec.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coveragePathIgnorePatterns: ["<rootDir>/__tests__/mock/"]
 };
