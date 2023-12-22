2f193608643c5593d1f04d58c00e3c7b7fb653c4
Vitor Scobin <vhscobin@gmail.com>
2023-12-22 00:30:00 -0300

    test: refact coverage specs and variations

--- tests/service.spec.ts
+++ tests/service.spec.ts
@@ -1,59 +1,94 @@
-import { ConnectionTypeEnum } from "../src/enums/service-criteria.enum";
+import { ConnectionTypeEnum, ConsumptionClassesEnum, TariffModalitiesEnum } from "../src/enums/service-criteria.enum";
+import { OutputCriteria } from "../src/interfaces/output-criteria.interface";
 import { eligibilityCriteria } from "../src/service";
 import { inputEligibility, outputEligibility } from "./mock/eligibility.mock";
-import { inputIneligible, outputIneligible } from "./mock/ineligible.mock";
 
-describe('Eligibility service', () => {
-  test('Should be return data eligibility', () => {
+describe("Eligibility - Connection type variations", () => {
+  test("Should be return data eligibility with biphasic", () => {
     const response = eligibilityCriteria(inputEligibility)
     expect(response).toEqual(outputEligibility)
   });
 
-  test('Should be return data ineligible', () => {
-    try {
-      eligibilityCriteria({
-        ...inputIneligible,
-        tipoDeConexao: ConnectionTypeEnum.singlePhase
-      })
-    } catch (error) {
-      expect(error).toEqual(outputIneligible)      
+  test("Should be return data eligibility with monoPhase", () => {
+    const response = eligibilityCriteria({...inputEligibility, tipoDeConexao: ConnectionTypeEnum.monoPhase})
+    expect(response).toEqual(outputEligibility)
+  });
+  test("Should be return data eligibility with threePhase", () => {
+    const response = eligibilityCriteria({...inputEligibility, tipoDeConexao: ConnectionTypeEnum.threePhase})
+    expect(response).toEqual(outputEligibility)
+  });
+});
+
+describe("Eligibility - Variations of reasons for ineligibility", () => {
+  test("Should return 'Consumption Classes' error", () => {
+    const outputIneligible:OutputCriteria = {
+      elegivel: false,
+      razoesDeInelegibilidade: [
+        "Classe de consumo não aceita",
+      ]
     }
+    expect(() => eligibilityCriteria(
+      { ...inputEligibility, classeDeConsumo: ConsumptionClassesEnum.rural }
+    )).toThrow(expect.objectContaining(outputIneligible))
   });
 
-  test('', () => {
-    try{
-      const response = eligibilityCriteria({
-        ...inputEligibility,
-        tipoDeConexao: '' as ConnectionTypeEnum
-      })
-    } catch (error) {
-      expect(error).toEqual({
-        ...outputIneligible,
-        razoesDeInelegibilidade: [
-         "Consumo muito baixo para tipo de conexão"
-        ]
-      })      
+  test("Should return 'Tariff Modality' error", () => {
+    const outputIneligible:OutputCriteria = {
+      elegivel: false,
+      razoesDeInelegibilidade: [
+        "Modalidade tarifária não aceita"
+      ]
     }
-  });
- 
-  test('Should be return "Minimum Customer Consumption" error', () => {
-    try {
-      eligibilityCriteria({
-        ...inputIneligible,
-        tipoDeConexao: ConnectionTypeEnum.threePhase,
-        historicoDeConsumo: [258,258,369]
-      })
-    } catch (error) {
-      expect(error).toEqual({
-        ...outputIneligible,
-        razoesDeInelegibilidade: [
-          "Classe de consumo não aceita",
-         "Modalidade tarifária não aceita",
-         "Consumo muito baixo para tipo de conexão"
-        ]
-      })      
-    }
+    expect(() => eligibilityCriteria(
+      { ...inputEligibility, modalidadeTarifaria: TariffModalitiesEnum.green }
+    )).toThrow(expect.objectContaining(outputIneligible))
   });
 
+  test("Should return 'Minimum Customer Consumption' error", () => {
+    const outputIneligible:OutputCriteria = {
+      elegivel: false,
+      razoesDeInelegibilidade: [
+        "Consumo muito baixo para tipo de conexão"
+      ]
+    }
+    expect(() => eligibilityCriteria(
+      { ...inputEligibility, historicoDeConsumo: [300, 300] }
+    )).toThrow(expect.objectContaining(outputIneligible))
+  });
+});
+
+describe("Eligibility - CO² calculation variations", () => {
+  test("Should calculate 'economiaAnualDeCO2' with projection with fewer items than the maximum", () => {
+    const response = eligibilityCriteria(inputEligibility)
+
+    expect(response.economiaAnualDeCO2).toEqual(5553.24)
+  });
   
+  test("Should calculate 'economiaAnualDeCO2' with projection with fewer items than the maximum - variation with exact values", () => {
+    const response = eligibilityCriteria({
+      ...inputEligibility,
+      historicoDeConsumo: [
+        600,
+        600,
+        600,
+        600
+      ]
+    })
+    expect(response.economiaAnualDeCO2).toEqual(604.8)
+  });
+
+  test("Should calculate 'economiaAnualDeCO2' with projection with fewer items than the maximum - variation with varying values", () => {
+    const response = eligibilityCriteria({
+      ...inputEligibility,
+      historicoDeConsumo: [
+        873,
+        750,
+        698,
+        732,
+        563
+      ]
+    })
+    
+    expect(response.economiaAnualDeCO2).toEqual(728.9856000000001)
+  });
 });
