63b6471ee89d6867add52d485bf9ef196ae58e04
Vitor Scobin <vhscobin@gmail.com>
2023-12-21 22:05:42 -0300

    feat: add enums

---
+++ src/enums/service-criteria.enum.ts
@@ -0,0 +1,21 @@
+export enum ConnectionTypeEnum {
+  singlePhase = 'monofasico',
+  biphasic = 'bifasico',
+  threePhase = 'trifasico'
+}
+
+export enum ConsumptionClassesEnum {
+  residential = 'residencial',
+  industrial = 'industrial',
+  commercial = 'comercial',
+  rural = 'rural',
+  publicAuthority = 'poderPublico',
+}
+
+export enum TariffModalitiesEnum {
+  blue = 'azul',
+  white = 'branca',
+  green = 'verde',
+  conventional = 'convencional'
+}
+
