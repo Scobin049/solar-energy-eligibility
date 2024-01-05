d1fa0a80cb617fac8f945f6bd7f9d72349064167
Vitor Scobin <vhscobin@gmail.com>
2024-01-05 19:19:21 -0300

    refact: include enums

--- src/enums/service-criteria.enum.ts
+++ src/enums/service-criteria.enum.ts
@@ -12,6 +12,32 @@ export enum ConsumptionClassesEnum {
   publicAuthority = 'poderPublico',
 }
 
+export enum IndustrialSubClassEnum {
+  industrial = 'Industrial'
+}
+
+export enum ResidentialSubClassEnum {
+  residential = 'Residencial',
+  lowIncome = 'Baixa renda'
+}
+
+export enum CommercialSubClassEnum {
+  condominiumAdministration = "Administração Condominal",
+  commercial = "Comercial",
+  telecommunicationServices = "Serviços de Telecomunicação",
+  religiousTemples = "Templos Religiosos"
+}
+
+export enum RuralSubClassEnum {
+  ruralFarming = "Agropecuária rural"
+}
+
+export enum PublicAuthoritySubClassEnum {
+  statePublicPower = 'Poder público estadual',
+  municipalPublicPower = 'Poder público municipal',
+}
+
+
 export enum TariffModalitiesEnum {
   blue = 'azul',
   white = 'branca',
