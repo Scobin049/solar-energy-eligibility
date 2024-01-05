c3d5441754ddb95f8db2bcab8e17551f3d321071
Vitor Scobin <vhscobin@gmail.com>
2024-01-05 19:20:19 -0300

    feat: include cpf/cnpj and subclass validations

--- src/service.ts
+++ src/service.ts
@@ -1,17 +1,33 @@
+import { cnpj, cpf } from 'cpf-cnpj-validator';
 import {
-  AVERAGE_CO2_EMISSION, KWH_CO2_EMISSION, MAX_RECENT_VALUES,
+  AVERAGE_CO2_EMISSION, CPF_LENGTH, KWH_CO2_EMISSION, MAX_RECENT_VALUES,
   MIN_BIPHASIC,
   MIN_MONOPHASE,
   MIN_THREEPHASIC,
   consumptionClassesError,
+  consumptionSubClassesError,
+  documentInvalidError,
   minimumCustomerConsumptionError,
   tariffModalityError
 } from "./constants";
-import { ConnectionTypeEnum, ConsumptionClassesEnum, TariffModalitiesEnum } from "./enums/service-criteria.enum";
+import { CommercialSubClassEnum, ConnectionTypeEnum, ConsumptionClassesEnum, IndustrialSubClassEnum, ResidentialSubClassEnum, TariffModalitiesEnum } from "./enums/service-criteria.enum";
 import { InputCriteria } from "./interfaces/input-criteria.interface";
 import { OutputCriteria } from "./interfaces/output-criteria.interface";
 
 export const eligibilityCriteria = (payload: InputCriteria): OutputCriteria => {
+  const {numeroDoDocumento} = payload;
+
+  let documentValid = false
+  if(numeroDoDocumento.length === CPF_LENGTH) {
+    documentValid = cpf.isValid(numeroDoDocumento);
+  } else {
+    documentValid = cnpj.isValid(numeroDoDocumento);
+  }
+
+  if(!documentValid) {
+    throw { elegivel: false, razoesDeInelegibilidade: [documentInvalidError] }; 
+  }
+
   const historyConsumption = payload.historicoDeConsumo.slice(0, MAX_RECENT_VALUES)
   const sumConsumption = historyConsumption.reduce((acc, num) => acc + num, 0);
   const averageConsumption = sumConsumption / historyConsumption.length;
@@ -35,17 +51,32 @@ const minimumCustomerConsumptionValidation = (averageConsumption: number, tipoDe
 const calculateAnnualSavingsProjection = (averageConsumption:number): number => {
   const annualProjection = averageConsumption * MAX_RECENT_VALUES;
   return (annualProjection * AVERAGE_CO2_EMISSION) / KWH_CO2_EMISSION;
-
 }
 
 const validateRules = (payload: InputCriteria, averageConsumption:number): void => {
-  const eligibleConsumptionClasses = [ConsumptionClassesEnum.residential, ConsumptionClassesEnum.industrial, ConsumptionClassesEnum.commercial];
-  const eligibleTariffModality = [TariffModalitiesEnum.white, TariffModalitiesEnum.conventional];  
-  const isMinimumCustomerConsumption = minimumCustomerConsumptionValidation(averageConsumption, payload.tipoDeConexao);
-
   const errors = [];
-  if(!eligibleConsumptionClasses.includes(payload.classeDeConsumo)) errors.push(consumptionClassesError);  
-  if(!eligibleTariffModality.includes(payload.modalidadeTarifaria)) errors.push(tariffModalityError);  
+  
+  const eligibleConsumptionClasses = [ConsumptionClassesEnum.residential, ConsumptionClassesEnum.industrial, ConsumptionClassesEnum.commercial];
+  if(!eligibleConsumptionClasses.includes(payload.classeDeConsumo) ) errors.push(consumptionClassesError);
+  
+  const subClassValition: {[key: string]: string[]} = {
+    [ConsumptionClassesEnum.residential]: [
+      ResidentialSubClassEnum.residential
+    ],
+    [ConsumptionClassesEnum.industrial]: [IndustrialSubClassEnum.industrial],
+    [ConsumptionClassesEnum.commercial]: [
+      CommercialSubClassEnum.condominiumAdministration,
+      CommercialSubClassEnum.commercial,
+      CommercialSubClassEnum.telecommunicationServices,
+    ],
+  }
+  const eligibleSubClasses = subClassValition[payload.classeDeConsumo]
+  const eligibleTariffModality = [TariffModalitiesEnum.white, TariffModalitiesEnum.conventional];  
+  if(!eligibleSubClasses || !eligibleSubClasses.includes(payload.subclasseDeConsumo)) errors.push(consumptionSubClassesError);
+  else if(!eligibleTariffModality.includes(payload.modalidadeTarifaria)) errors.push(tariffModalityError);
+  
+  const isMinimumCustomerConsumption = minimumCustomerConsumptionValidation(averageConsumption, payload.tipoDeConexao);
   if(!isMinimumCustomerConsumption) errors.push(minimumCustomerConsumptionError);
+  
   if(!!errors.length) throw { elegivel: false, razoesDeInelegibilidade: errors }; 
 }
